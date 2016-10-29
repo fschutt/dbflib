1e96679e83166ae8cf32fc5750e17716a7e67ea7
Felix Schütt <felixschuett@outlook.de>
2016-10-29 19:48:19 +0200

    jetzt aber

--- include/DBaseFile.h
+++ include/DBaseFile.h
@@ -3,129 +3,117 @@
 
 #include <string>
 #include <vector>
-#include <stdint.h>
-#include <bitset>
-#include <ctime>
-#include <exception>
-#include <stdexcept>
 
 #include "DBaseRecord.h"
 #include "DBaseFieldDescArray.h"
+#include "DBaseHeader.h"
 
 class DBaseFile
 {
     public:
-        bool openFile(const std::string fileName);              //Open file and get contents
-		void stat();                                            //Output debug information
+        /**<Open file and get contents */
+        bool openFile(const std::string fileName);
+        /**<Open file and get contents */
+		void stat(){header.stat();};
 
     private:
-		std::string getFileContents(std::ifstream& iFile);              //Read file contents safely into std::string
-        int calculateNextBlockSize(int prev, int totalStringSize, ...); //On failed block size, calculate next
+        /**< Read file contents safely into std::string */
+		std::string getHeader(std::ifstream& iFile);
 
-    	//Helper function: converts char to bitset
-    	std::bitset<8> ToBits(const char& byte);
-        //File contents
-		std::string m_rawData = "";
-		int fileSizeBytes = 0;
-		unsigned int blockSize = 32;        //max be 16 or 32 or 48 Byte
+        std::string readRecords(std::ifstream& iFile);
+        /**< Set block size depending on found block size: Either 48 Byte, 32 Byte or 16 Byte */
+        int calculateNextBlockSize(int prev, int totalStringSize, ...);
 
-        //--------------------------------------------------
-        //Level 5 DOS Header for dBase III - dBase VII files
-        //--------------------------------------------------
-        std::string fileType;
-        char firstByte;                     //BYPTE 0 temporary storage, c++ can only read bytes
-            uint16_t versionNr = 3;         //BYTE 0 bit 0-2 version number of file
-            bool memoFilePresent = false;   //BYTE 0 bit 3 is a memo present or not
-            bool sqlFilePresent = false;    //BYTE 0 bit 4-6 is a .sql file present or not
-            bool anyMemoFilePresent = false;//BYTE 0 bit 7 is one of the above true
-        time_t lastUpdated = time(0);       //BYTE 1-3 YYMMDD format
-        uint32_t numRecordsInDB = 0;        //BYTE 4-7 32-bit nr of records
-        uint16_t numBytesInHeader = 0;      //BYTE 8-9 16 bit nr of bytes in header
-        uint16_t numBytesInRecord = 0;      //BYTE 10-11 16 bit nr of bytes in record
-                                            //BYTE 12-13: 0, ignore
-        bool incompleteTransaction = false; //BYTE 14, set by BEGIN TRANSACTION
-        bool encrypted = false;             //BYTE 15, dBase IV encryption flag
-                                            //BYTE 16-27 dBase DOS in multi-user environment
-        bool productionMDX = false;         //BYTE 28 0x01 = production .mdx available, else not
-                                            //BYTE 30-31:0, ignore
-											//BYTE 64-67:0, ignore
-                                            //BYTE n+1: 0x0D (13) field descriptor terminator
-                                            //BYTE n+2 start of the field properties
+		/**< File size of .dbf file */
+		int m_fileSize = 0;
+		/**< File header length in Bytes */
+		unsigned int m_fileHeaderLength = 32;
+		/**< Field subrecords structure */
+		unsigned int m_subrecordsLength = 0;
+		/**< Total header size (fileHeader + subrecords definition) */
+		unsigned int m_totalHeaderLength = 0;
+        /**< Header contents (read raw from disk)*/
+		std::string m_headerData = "";
 
-
+		/**< \section Member variables */
+		/**< Header structure */
+		DBaseHeader header;
+		/**< Subrecords (column definitions) */
         std::vector<DBaseFieldDescArray> fieldDescriptors;	//BYTE 68-n Field Descriptor Array, 48 bytes each
-        unsigned int endOfHeader = 0;
-		bool hasStructuralCDX = false;
-		bool hasMemoField = false;
-		bool isDatabase =  false;
-		uint8_t fieldDescArrayNum = 0;
-		uint8_t codePageMark = 0;
 };
 
-//Exceptions
-//no memory available on system
+/**< \section   Exceptions */
+/**< \brief     There is not enough memory available on the target computer */
 class noMemoryAvailableEx : public std::exception{
     std::string m_defaultErrorStr;
 public:
     noMemoryAvailableEx(std::string errorStr = "Out of memory") : m_defaultErrorStr(errorStr){};
     ~noMemoryAvailableEx();
+    virtual const char* what() const noexcept{ return (m_defaultErrorStr.c_str());};
 };
 
-//file could not be read
-class fileNotFoundEx : public std::runtime_error{
-    static constexpr const char* m_defaultErrorStr = "File not found";
+/**< \brief     File could not be found or user has no access to file */
+class fileNotFoundEx : public std::exception{
+    std::string m_defaultErrorStr;
 public:
-    fileNotFoundEx(const char* errorStr = m_defaultErrorStr);
+    fileNotFoundEx(std::string errorStr = "File could not be opened") : m_defaultErrorStr(errorStr){};
     ~fileNotFoundEx();
+    virtual const char* what() const noexcept{ return (m_defaultErrorStr.c_str());};
 };
 
-//header is not a calculatable, misaligned bytes
-class unexpectedHeaderEndEx : public std::runtime_error{
-    friend class unexpectedBlockSize;
+/**< \brief     Header is corrupt, 0x0D encountered too early*/
+class unexpectedHeaderEndEx : public std::exception{
 protected:
-	static constexpr int m_byteHeaderFailed = 0;
-	static constexpr bool m_isFoxBaseHeader = false;
-	static constexpr const char* m_defaultErrorStr = "Unexpected header termination";
+	std::string m_defaultErrorStr;
+	unsigned int m_byteHeaderFailed = 0;
+	bool m_isFoxBaseHeader = false;
 public:
-    unexpectedHeaderEndEx(const char* errorStr = m_defaultErrorStr,
-                          unsigned int byteWhenFailed = m_byteHeaderFailed,
-                          bool isFoxBaseHeader = m_isFoxBaseHeader)
-                          : runtime_error(m_defaultErrorStr){};
+    unexpectedHeaderEndEx(std::string errorStr = "Unexpected header termination",
+                          unsigned int byteWhenFailed = 0,
+                          bool isFoxBaseHeader = false)
+                          : m_defaultErrorStr(errorStr),
+                            m_byteHeaderFailed(byteWhenFailed),
+                            m_isFoxBaseHeader(isFoxBaseHeader){};
     virtual ~unexpectedHeaderEndEx();
-    what(const char* errorStr = m_defaultErrorStr);
+    virtual const char* what() const noexcept{ return (m_defaultErrorStr.c_str());};
 };
 
-//blockSize is too short or too long, calculate expected byte layout
+/**< \brief     Subrecords (column definitions) do not fit neatly in header.
+*               Try again with different block size */
 class unexpectedBlockSize : protected unexpectedHeaderEndEx{
-    static constexpr const char* m_defaultErrorStr = "Unexpected block size";
-	static constexpr unsigned int m_unexpectedBlockSizePrev = 32;
+	unsigned int m_blockSizeDefault = 48;
+	unsigned int m_blockSizeCurrent = m_blockSizeDefault;
 public:
-    unexpectedBlockSize(const char* errorStr = m_defaultErrorStr,
-                        unsigned int byteWhenFailed = m_byteHeaderFailed,
-                        unsigned int unexpectedBlockSizePrev = m_unexpectedBlockSizePrev)
-                        : unexpectedHeaderEndEx(errorStr){};
-    ~unexpectedBlockSize();
+    unexpectedBlockSize(std::string errorStr = "Unexpected block size",
+                        unsigned int byteWhenFailed = 0,
+                        unsigned int blockSizeCurrent = 48)
+                        : unexpectedHeaderEndEx(errorStr, byteWhenFailed, false){
+                              if(blockSizeCurrent != m_blockSizeDefault){
+                                    m_blockSizeCurrent = blockSizeCurrent;
+                                    }
+                          };
+    virtual ~unexpectedBlockSize();
+    virtual const char* what() const noexcept{ return (m_defaultErrorStr.c_str());};
 };
 
-//base for other file-related exceptions
-class badFileEx : public std::runtime_error{
-    friend class incompleteTransactionEx;
+/**< \brief     File could not be opened*/
+class badFileEx : public std::exception{
 protected:
-	static constexpr const char* m_defaultErrorStr = "Unexpected error when opening file.";
+	std::string m_defaultErrorStr;
 public:
-    badFileEx(const char* errorStr = m_defaultErrorStr);
-    ~badFileEx();
-	what(const char* errorStr = m_defaultErrorStr);
+    badFileEx(std::string errorStr = "Unexpected error when opening file")
+              : m_defaultErrorStr(errorStr){};
+    virtual ~badFileEx();
+    virtual const char* what() const noexcept{ return (m_defaultErrorStr.c_str());};
 };
 
-//transaction is incomplete
+/**< \brief     Transaction is incomplete, meaning database is possibly corrupt.*/
 class incompleteTransactionEx : public badFileEx{
-private:
-    static constexpr const char* m_defaultErrorStr = "File transaction is incomplete.";
 public:
-    incompleteTransactionEx(const char* errorStr = m_defaultErrorStr)
+    incompleteTransactionEx(std::string errorStr = "File transaction is incomplete")
                             : badFileEx(errorStr){};
     ~incompleteTransactionEx();
+    virtual const char* what() const noexcept{ return (m_defaultErrorStr.c_str());};
 };
 
 #endif // DBASEFILE_H
---
+++ include/DBaseHeader.h
@@ -0,0 +1,50 @@
+#ifndef DBASEHEADER_H
+#define DBASEHEADER_H
+#include <fstream>
+#include <stdint.h>
+#include <bitset>
+#include <ctime>
+
+class DBaseHeader
+{
+    public:
+        DBaseHeader();
+        ~DBaseHeader();
+        bool parse(std::string& headerData);
+        void stat();
+
+        //--------------------------------------------------
+        //Level 7 DOS Header for dBase III - dBase VII files
+        //--------------------------------------------------
+        unsigned int m_blockSize = 32;
+        bool m_hasStructuralCDX = false;
+		bool m_hasMemoField = false;
+		bool m_isDatabase =  false;
+		uint8_t m_fieldDescArrayNum = 0;
+		uint8_t m_codePageMark = 0;
+
+        std::string m_fileType;
+        char m_firstByte;                       //BYPTE 0 temporary storage, c++ can only read bytes
+            uint16_t m_versionNr = 3;           //BYTE 0 bit 0-2 version number of file
+            bool m_memoFilePresent = false;     //BYTE 0 bit 3 is a memo present or not
+            bool m_sqlFilePresent = false;      //BYTE 0 bit 4-6 is a .sql file present or not
+            bool m_anyMemoFilePresent = false;  //BYTE 0 bit 7 is one of the above true
+        time_t m_lastUpdated = time(0);         //BYTE 1-3 YYMMDD format
+        uint32_t m_numRecordsInDB = 0;          //BYTE 4-7 32-bit nr of records
+        uint16_t m_numBytesInHeader = 0;        //BYTE 8-9 16 bit nr of bytes in header
+        uint16_t m_numBytesInRecord = 0;        //BYTE 10-11 16 bit nr of bytes in record
+                                                //BYTE 12-13: 0, ignore
+        bool m_incompleteTransaction = false;   //BYTE 14, set by BEGIN TRANSACTION
+        bool m_encrypted = false;               //BYTE 15, dBase IV encryption flag
+                                                //BYTE 16-27 dBase DOS in multi-user environment
+        bool m_productionMDX = false;           //BYTE 28 0x01 = production .mdx available, else not
+                                                //BYTE 30-31:0, ignore
+                                                //BYTE 64-67:0, ignore
+                                                //BYTE n+1: 0x0D (13) field descriptor terminator
+                                                //BYTE n+2 start of the field properties
+    private:
+        /**< Helper function: converts char to bitset */
+    	std::bitset<8> toBits(const char& byte);
+};
+
+#endif // DBASEHEADER_H
